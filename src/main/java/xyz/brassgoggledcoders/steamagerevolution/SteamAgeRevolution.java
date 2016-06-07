5515632cd2781e62c9c02d1063e4777a49c681fc
warlordjones <warlordjones@gmail.com>
2016-06-08 00:42:32 +0100

    Loading override thingy

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/SteamAgeRevolution.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/SteamAgeRevolution.java
@@ -3,17 +3,14 @@ package xyz.brassgoggledcoders.steamagerevolution;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import xyz.brassgoggledcoders.boilerplate.BaseCreativeTab;
 import xyz.brassgoggledcoders.boilerplate.BoilerplateModBase;
-import xyz.brassgoggledcoders.boilerplate.proxies.CommonProxy;
-import xyz.brassgoggledcoders.steamagerevolution.modules.parts.PartsModule;
 import xyz.brassgoggledcoders.steamagerevolution.modules.rawmaterials.RawMaterialsModule;
-import xyz.brassgoggledcoders.steamagerevolution.modules.tea.TeaModule;
-import xyz.brassgoggledcoders.steamagerevolution.modules.vanity.VanityModule;
 
 @Mod(modid = SteamAgeRevolution.MODID, name = SteamAgeRevolution.MODNAME, version = SteamAgeRevolution.MODVERSION)
 public class SteamAgeRevolution extends BoilerplateModBase
@@ -30,21 +27,43 @@ public class SteamAgeRevolution extends BoilerplateModBase
 	public static final String MODVERSION = "@VERSION@";
 
 	public static CreativeTabs tab = new SARTab();
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		super.preInit(event);
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		super.init(event);
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		super.postInit(event);
+	}
 
 	@Override
-	protected void modPreInit(FMLPreInitializationEvent event) {
+	public void modPreInit(FMLPreInitializationEvent event)
+	{
+
+	}
+
+	@Override
+	public void modInit(FMLInitializationEvent event)
+	{
+
+	}
+
+	@Override
+	protected void modPostInit(FMLPostInitializationEvent event)
+	{
 		
 	}
 
-	@Override
-	protected void modInit(FMLInitializationEvent event) {
-		
-	}
-
-	@Override
-	protected void modPostInit(FMLPostInitializationEvent event) {
-		logger.devError("" + this.getModuleHandler().getModules());
-	}
 	
 	@Override
 	public Object getInstance()
