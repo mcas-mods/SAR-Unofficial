c673dca5c2533c0a652540c2a3b9b46f9b07bf11
warlordjones <warlordjones@gmail.com>
2016-07-09 22:25:33 +0100

    I have no idea if this oregen is balanced.

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/materials/ModuleMaterials.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/materials/ModuleMaterials.java
@@ -9,22 +9,22 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import xyz.brassgoggledcoders.boilerplate.Boilerplate;
 import xyz.brassgoggledcoders.boilerplate.blocks.material.BlockMetal;
 import xyz.brassgoggledcoders.boilerplate.blocks.material.BlockMetalOre;
 import xyz.brassgoggledcoders.boilerplate.items.ItemSubBase;
 import xyz.brassgoggledcoders.boilerplate.module.Module;
 import xyz.brassgoggledcoders.boilerplate.module.ModuleBase;
 import xyz.brassgoggledcoders.boilerplate.recipes.RecipeUtils;
+import xyz.brassgoggledcoders.boilerplate.worldgen.OreGenerationHandler;
 import xyz.brassgoggledcoders.steamagerevolution.SteamAgeRevolution;
 
 @Module(mod = SteamAgeRevolution.MODID)
 public class ModuleMaterials extends ModuleBase {
 	public static Item dust, plate;
 	public static ItemSubBase bearing, bolt, gear, nail, nut, screw, spring, sprocket, thread, washer;
-	public static List<String> metalsParts = Arrays.asList("iron", "brass");
 
 	public static List<String> oreMetals = Arrays.asList(BlockMetalOre.EnumBlockType.names());
 	public static List<String> ourMetals = Arrays.asList(BlockMetal.EnumBlockType.names());
@@ -32,9 +32,12 @@ public class ModuleMaterials extends ModuleBase {
 			Arrays.asList(ArrayUtils.addAll(BlockMetalOre.EnumBlockType.names(), new String[] {"iron"}));
 	public static List<String> allMetals =
 			Arrays.asList(ArrayUtils.addAll(BlockMetalOre.EnumBlockType.names(), new String[] {"iron", "gold"}));
+	public static List<String> partsMetals = Arrays.asList("iron", "brass");
 
 	public static Block metal_ore, metal_block;
 	public static Item ingot, nugget;
+
+	// TODO oredict registration
 
 	@Override
 	public void preInit(FMLPreInitializationEvent fmlPreInitializationEvent) {
@@ -45,19 +48,19 @@ public class ModuleMaterials extends ModuleBase {
 		getItemRegistry().registerItem(plate);
 
 		// TODO Old method.
-		bearing = new ItemSubBase("parts", "bearing", metalsParts);
+		bearing = new ItemSubBase("parts", "bearing", partsMetals);
 		this.getItemRegistry().registerItem(bearing);
-		bolt = new ItemSubBase("parts", "bolt", metalsParts);
+		bolt = new ItemSubBase("parts", "bolt", partsMetals);
 		this.getItemRegistry().registerItem(bolt);
-		gear = new ItemSubBase("parts", "gear", metalsParts);
+		gear = new ItemSubBase("parts", "gear", partsMetals);
 		this.getItemRegistry().registerItem(gear);
-		nail = new ItemSubBase("parts", "nail", metalsParts);
+		nail = new ItemSubBase("parts", "nail", partsMetals);
 		this.getItemRegistry().registerItem(nail);
-		nut = new ItemSubBase("parts", "nut", metalsParts);
+		nut = new ItemSubBase("parts", "nut", partsMetals);
 		this.getItemRegistry().registerItem(nut);
-		screw = new ItemSubBase("parts", "screw", metalsParts);
+		screw = new ItemSubBase("parts", "screw", partsMetals);
 		this.getItemRegistry().registerItem(screw);
-		spring = new ItemSubBase("parts", "spring", metalsParts);
+		spring = new ItemSubBase("parts", "spring", partsMetals);
 		this.getItemRegistry().registerItem(spring);
 
 		metal_ore = new BlockMetalOre();
@@ -70,7 +73,10 @@ public class ModuleMaterials extends ModuleBase {
 		nugget = new ItemSubBase("metals/", "nugget", almostAllMetals);
 		getItemRegistry().registerItem(nugget);
 
-		this.getConfigRegistry().addNewConfigFile("oregeneration");
+		// TODO This needs to go someplace else.
+		Boilerplate.instance.getRegistryHolder().getConfigRegistry().addNewConfigFile("oregeneration");
+		OreGenerationHandler.requestOreGeneration("Copper", metal_ore.getStateFromMeta(0), 5, 70, 2);
+		OreGenerationHandler.requestOreGeneration("Zinc", metal_ore.getStateFromMeta(1), 3, 50, 1);
 	}
 
 	@Override
@@ -84,11 +90,6 @@ public class ModuleMaterials extends ModuleBase {
 		for(int i = 0; i < BlockMetal.EnumBlockType.values().length; i++)
 			// TODO Plate/Dust creation recipes
 			GameRegistry.addSmelting(new ItemStack(ingot, 1, i), new ItemStack(dust, 1, i), 0.5F);
-	}
-
-	@Override
-	public void postInit(FMLPostInitializationEvent event) {
-		GameRegistry.registerWorldGenerator(new OreGenerationHandler(), 5);
 	}
 
 	@Override
