849e74cbcea5556e60c7f1adc304a167b8bf7519
warlordjones <warlordjones@gmail.com>
2016-07-12 20:50:38 +0100

    Belt dummy work

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/mechanical/blocks/BlockBeltDummy.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/mechanical/blocks/BlockBeltDummy.java
@@ -1,6 +1,12 @@
 package xyz.brassgoggledcoders.steamagerevolution.modules.mechanical.blocks;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import xyz.brassgoggledcoders.boilerplate.blocks.BlockBase;
 
 public class BlockBeltDummy extends BlockBase {
@@ -8,6 +14,23 @@ public class BlockBeltDummy extends BlockBase {
 	public BlockBeltDummy(Material mat, String name) {
 		super(mat, name);
 		this.setBlockUnbreakable();
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos,
+			EnumFacing side) {
+		return true;
 	}
 
 }
