a1fbe0be6704c302a0e7c83efc284392482d06bb
warlordjones <warlordjones@gmail.com>
2017-02-15 00:27:07 +0000

    Fix a crash when colliding with an empty casting bench

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/steam/blocks/BlockCastingBench.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/steam/blocks/BlockCastingBench.java
@@ -83,7 +83,8 @@ public class BlockCastingBench extends BlockTEBase<TileEntityCastingBench> {
 	public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
 		TileEntityCastingBench te = this.getTileEntity(worldIn, pos);
 		if(te != null) {
-			if(te.tank.getFluid().getFluid().getTemperature() > FluidRegistry.WATER.getTemperature()) {
+			if(te.tank.getFluid() != null
+					&& te.tank.getFluid().getFluid().getTemperature() > FluidRegistry.WATER.getTemperature()) {
 				entityIn.setFire(10);
 			}
 		}
