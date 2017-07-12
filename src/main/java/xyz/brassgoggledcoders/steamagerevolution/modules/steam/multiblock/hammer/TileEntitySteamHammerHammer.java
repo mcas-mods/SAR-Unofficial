0e628ba9abf9e90add2683fffceced7deb16bca8
warlordjones <warlordjones@gmail.com>
2017-07-12 23:02:50 +0100

    Some work at fluid tanks accepting fluid from items

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/steam/multiblock/hammer/TileEntitySteamHammerHammer.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/steam/multiblock/hammer/TileEntitySteamHammerHammer.java
@@ -1,10 +1,33 @@
 package xyz.brassgoggledcoders.steamagerevolution.modules.steam.multiblock.hammer;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import com.teamacronymcoders.base.multiblock.validation.IMultiblockValidator;
+
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.items.CapabilityItemHandler;
 
 public class TileEntitySteamHammerHammer extends TileEntitySteamHammerPart {
 	@Override
 	public boolean isGoodForTop(IMultiblockValidator validatorCallback) {
 		return true;
+	}
+
+	// TODO This doesn't make sense. Should be in the anvil. But must be vanilla useable...
+	@Override
+	public boolean hasCapability(@Nonnull Capability<?> capability, @Nullable EnumFacing facing) {
+		return (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY && this.isConnected())
+				|| super.hasCapability(capability, facing);
+	}
+
+	@Override
+	@Nonnull
+	public <T> T getCapability(@Nonnull Capability<T> capability, @Nullable EnumFacing facing) {
+		if(capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+			return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.cast(this.getMultiblockController().inventory);
+		}
+		return super.getCapability(capability, facing);
 	}
 }
--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/storage/blocks/BlockBasicFluidTank.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/storage/blocks/BlockBasicFluidTank.java
@@ -4,13 +4,19 @@ import com.teamacronymcoders.base.blocks.BlockTEBase;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import xyz.brassgoggledcoders.steamagerevolution.modules.storage.tileentities.TileEntityBasicFluidTank;
@@ -55,5 +61,26 @@ public class BlockBasicFluidTank extends BlockTEBase<TileEntityBasicFluidTank> {
 	@Override
 	public EnumBlockRenderType getRenderType(IBlockState state) {
 		return EnumBlockRenderType.MODEL;
+	}
+
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
+			EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		TileEntityBasicFluidTank te = getTileEntity(worldIn, pos);
+		if(te != null && !playerIn.isSneaking()) {
+			ItemStack held = playerIn.getHeldItem(hand);
+			if(held.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null)) {
+				// TODO Reverse operation
+				IFluidHandlerItem itemHandler =
+						held.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
+				IFluidHandler tankHandler = te.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, facing);
+				// TODO Don't drain if fluid would be wasted.
+				if(itemHandler.drain((tankHandler.getTankProperties()[0].getCapacity()), false) != null) {
+					tankHandler.fill(itemHandler.drain(tankHandler.getTankProperties()[0].getCapacity(), true), true);
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
