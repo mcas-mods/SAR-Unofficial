7b7f2cf307e1bdac85f039487e7751de080eed4b
warlordjones <warlordjones@gmail.com>
2017-07-28 15:04:51 +0100

    Some resource work

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/SteamAgeRevolution.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/SteamAgeRevolution.java
@@ -1,12 +1,17 @@
 package xyz.brassgoggledcoders.steamagerevolution;
 
+import java.io.File;
+
 import com.teamacronymcoders.base.BaseModFoundation;
+import com.teamacronymcoders.base.registrysystem.config.ConfigRegistry;
+import com.teamacronymcoders.base.util.Platform;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -72,6 +77,21 @@ public class SteamAgeRevolution extends BaseModFoundation<SteamAgeRevolution> {
 	@Override
 	public SteamAgeRevolution getInstance() {
 		return instance;
+	}
+
+	@Override
+	public File getResourceFolder() {
+		if(!Platform.isDevEnv())
+			return null;
+		else {
+			File file = new File(
+					this.getRegistry(ConfigRegistry.class, "CONFIG").getConfigFolder().getParentFile().getParentFile()
+							.getParentFile().getParentFile().getPath(),
+					"src/main/resources/assets/" + this.getID() + "/");
+			FMLLog.bigWarning(file.getAbsolutePath());
+
+			return file;
+		}
 	}
 
 	public static class SARTab extends CreativeTabs {
--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/materials/ModuleMaterials.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/materials/ModuleMaterials.java
@@ -15,18 +15,22 @@ import com.teamacronymcoders.base.modulesystem.Module;
 import com.teamacronymcoders.base.modulesystem.ModuleBase;
 
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import scala.actors.threadpool.Arrays;
 import xyz.brassgoggledcoders.steamagerevolution.SteamAgeRevolution;
 import xyz.brassgoggledcoders.steamagerevolution.utils.MaterialPartMetalBlock;
 
 @Module(value = SteamAgeRevolution.MODID)
 public class ModuleMaterials extends ModuleBase {
 
-	String[] alloyParts = new String[] {"gear", "ingot", "nugget", "plate"};
+	String[] vanillaParts = new String[] {"nugget", "plate", "dust"};
+	String[] alloyParts = new String[] {"ingot", "nugget", "plate", "dust"};
 
 	@Override
 	public void preInit(FMLPreInitializationEvent event) {
 		try {
 			MaterialUser SAR = SteamAgeRevolution.instance.getMaterialUser();
+			Material iron = new MaterialBuilder().setName("Iron").setColor(Color.GRAY).setHasEffect(false).build();
+			Material gold = new MaterialBuilder().setName("Gold").setColor(Color.YELLOW).setHasEffect(false).build();
 			Material steel =
 					new MaterialBuilder().setName("Steel").setColor(Color.DARK_GRAY).setHasEffect(false).build();
 			Material brass = new MaterialBuilder().setName("Brass").setColor(Color.ORANGE).setHasEffect(false).build();
@@ -34,9 +38,16 @@ public class ModuleMaterials extends ModuleBase {
 			Part moltenMetal = new PartBuilder().setName("metal_molten").setPartType(new FluidPartType()).build();
 			Part storageMetal = new PartBuilder().setName("metal_storage").setPartType(new BlockPartType()).build();
 
+			MaterialPart moltenIron = new MaterialPart(SAR, iron, moltenMetal);
+			MaterialPart moltenGold = new MaterialPart(SAR, gold, moltenMetal);
 			MaterialPart moltenBrass = new MaterialPart(SAR, brass, moltenMetal);
 			MaterialPart moltenSteel = new MaterialPart(SAR, steel, moltenMetal);
 
+			SAR.registerPartsForMaterial(iron, vanillaParts);
+			SAR.registerMaterialPart(moltenIron);
+			// Gold already has a nugget
+			SAR.registerPartsForMaterial(gold, (String[]) Arrays.copyOfRange(vanillaParts, 1, vanillaParts.length));
+			SAR.registerMaterialPart(moltenGold);
 			SAR.registerPartsForMaterial(steel, alloyParts);
 			SAR.registerMaterialPart(moltenSteel);
 			SAR.registerMaterialPart(new MaterialPartMetalBlock(SAR, brass, storageMetal));
