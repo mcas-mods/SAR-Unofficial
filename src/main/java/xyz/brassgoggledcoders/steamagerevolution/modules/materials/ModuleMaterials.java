21dcad2d134e7d39c1452a4408292ffe64a0571d
warlordjones <warlordjones@gmail.com>
2017-07-29 16:21:37 +0100

    Add zinc and copper

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/materials/ModuleMaterials.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/materials/ModuleMaterials.java
@@ -4,56 +4,44 @@ import java.awt.Color;
 
 import com.teamacronymcoders.base.materialsystem.MaterialException;
 import com.teamacronymcoders.base.materialsystem.MaterialUser;
-import com.teamacronymcoders.base.materialsystem.materialparts.MaterialPart;
 import com.teamacronymcoders.base.materialsystem.materials.Material;
 import com.teamacronymcoders.base.materialsystem.materials.MaterialBuilder;
-import com.teamacronymcoders.base.materialsystem.parts.Part;
-import com.teamacronymcoders.base.materialsystem.parts.PartBuilder;
-import com.teamacronymcoders.base.materialsystem.parttype.BlockPartType;
-import com.teamacronymcoders.base.materialsystem.parttype.FluidPartType;
 import com.teamacronymcoders.base.modulesystem.Module;
 import com.teamacronymcoders.base.modulesystem.ModuleBase;
 
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import scala.actors.threadpool.Arrays;
 import xyz.brassgoggledcoders.steamagerevolution.SteamAgeRevolution;
-import xyz.brassgoggledcoders.steamagerevolution.utils.MaterialPartMetalBlock;
 
 @Module(value = SteamAgeRevolution.MODID)
 public class ModuleMaterials extends ModuleBase {
 
-	String[] vanillaParts = new String[] {"nugget", "plate", "dust"};
-	String[] alloyParts = new String[] {"ingot", "nugget", "plate", "dust"};
+	String[] vanillaParts = new String[] {"plate", "dust", "fluid"};
+	String[] metalParts = new String[] {"ore", "ingot", "nugget", "plate", "dust", "block", "fluid"};
+	String[] alloyParts = new String[] {"ingot", "nugget", "plate", "dust", "block", "fluid"};
 
 	@Override
 	public void preInit(FMLPreInitializationEvent event) {
 		try {
 			MaterialUser SAR = SteamAgeRevolution.instance.getMaterialUser();
+
 			Material iron = new MaterialBuilder().setName("Iron").setColor(Color.GRAY).setHasEffect(false).build();
 			Material gold = new MaterialBuilder().setName("Gold").setColor(Color.YELLOW).setHasEffect(false).build();
+
+			Material copper = new MaterialBuilder().setName("Copper").setColor(new Color(159, 61, 19))
+					.setHasEffect(false).build();
+			Material zinc = new MaterialBuilder().setName("Zinc").setColor(new Color(133, 153, 154)).setHasEffect(false)
+					.build();
+
 			Material steel =
 					new MaterialBuilder().setName("Steel").setColor(Color.DARK_GRAY).setHasEffect(false).build();
 			Material brass = new MaterialBuilder().setName("Brass").setColor(Color.ORANGE).setHasEffect(false).build();
 
-			Part moltenMetal = new PartBuilder().setName("metal_molten").setPartType(new FluidPartType()).build();
-			Part storageMetal = new PartBuilder().setName("metal_storage").setPartType(new BlockPartType()).build();
-
-			MaterialPart moltenIron = new MaterialPart(SAR, iron, moltenMetal);
-			MaterialPart moltenGold = new MaterialPart(SAR, gold, moltenMetal);
-			MaterialPart moltenBrass = new MaterialPart(SAR, brass, moltenMetal);
-			MaterialPart moltenSteel = new MaterialPart(SAR, steel, moltenMetal);
-
 			SAR.registerPartsForMaterial(iron, vanillaParts);
-			SAR.registerMaterialPart(moltenIron);
-			// Gold already has a nugget
-			SAR.registerPartsForMaterial(gold, (String[]) Arrays.copyOfRange(vanillaParts, 1, vanillaParts.length));
-			SAR.registerMaterialPart(moltenGold);
+			SAR.registerPartsForMaterial(gold, vanillaParts);
+			SAR.registerPartsForMaterial(copper, metalParts);
+			SAR.registerPartsForMaterial(zinc, metalParts);
 			SAR.registerPartsForMaterial(steel, alloyParts);
-			SAR.registerMaterialPart(moltenSteel);
-			SAR.registerMaterialPart(new MaterialPartMetalBlock(SAR, brass, storageMetal));
 			SAR.registerPartsForMaterial(brass, alloyParts);
-			SAR.registerMaterialPart(moltenBrass);
-			SAR.registerMaterialPart(new MaterialPartMetalBlock(SAR, steel, storageMetal));
 		}
 		catch(MaterialException e) {
 			// TODO Auto-generated catch block
