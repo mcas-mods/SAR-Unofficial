a4a1f95bf65b3533c8eda7219d857d5f0b5af5c0
warlordjones <warlordjones@gmail.com>
2017-10-04 22:32:47 +0100

    More playing with JEI

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/compat/jei/steamhammer/SteamHammerRecipeCategory.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/compat/jei/steamhammer/SteamHammerRecipeCategory.java
@@ -2,18 +2,31 @@ package xyz.brassgoggledcoders.steamagerevolution.compat.jei.steamhammer;
 
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.gui.IDrawable;
+import mezz.jei.api.gui.IDrawableStatic;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.IRecipeCategory;
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import xyz.brassgoggledcoders.steamagerevolution.SteamAgeRevolution;
 
 public class SteamHammerRecipeCategory implements IRecipeCategory<SteamHammerRecipeWrapper> {
 
+	protected final IDrawableStatic hammer;
 	private final IGuiHelper helper;
 
 	public SteamHammerRecipeCategory(IGuiHelper helper) {
 		this.helper = helper;
+		hammer = helper.createDrawable(new ResourceLocation(SteamAgeRevolution.MODID, "textures/gui/hammer.png"), 0, 0,
+				32, 32, 32, 32);
+	}
+
+	@Override
+	public void drawExtras(Minecraft minecraft) {
+		hammer.draw(minecraft, 120, 100);
+		helper.getSlotDrawable().draw(minecraft, 100, 100);
+		helper.getSlotDrawable().draw(minecraft, 160, 100);
 	}
 
 	@Override
@@ -39,8 +52,8 @@ public class SteamHammerRecipeCategory implements IRecipeCategory<SteamHammerRec
 	@Override
 	public void setRecipe(IRecipeLayout recipeLayout, SteamHammerRecipeWrapper recipeWrapper,
 			IIngredients ingredients) {
-		recipeLayout.getItemStacks().init(0, true, 100, 60);
-		recipeLayout.getItemStacks().init(1, false, 125, 60);
+		recipeLayout.getItemStacks().init(0, true, 100, 100);
+		recipeLayout.getItemStacks().init(1, false, 160, 100);
 
 		recipeLayout.getItemStacks().set(0, ingredients.getInputs(ItemStack.class).get(0));
 		recipeLayout.getItemStacks().set(1, ingredients.getOutputs(ItemStack.class).get(0));
