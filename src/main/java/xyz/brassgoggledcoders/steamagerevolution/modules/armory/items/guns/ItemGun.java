856070adc3ed080d192ed986860863f314aebad6
warlordjones <warlordjones@gmail.com>
2019-02-06 23:58:19 +0000

    Implement automatic fire for guns

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/armory/items/guns/ItemGun.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/armory/items/guns/ItemGun.java
@@ -20,6 +20,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import xyz.brassgoggledcoders.steamagerevolution.SteamAgeRevolution;
 import xyz.brassgoggledcoders.steamagerevolution.modules.armory.entities.EntityBullet;
 import xyz.brassgoggledcoders.steamagerevolution.modules.armory.items.guns.IGunPart.GunPartType;
+import xyz.brassgoggledcoders.steamagerevolution.modules.armory.items.guns.IMechanism.ActionType;
 
 public class ItemGun extends ItemBase {
 
@@ -34,6 +35,10 @@ public class ItemGun extends ItemBase {
 	@SideOnly(Side.CLIENT)
 	@ParametersAreNonnullByDefault
 	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		tooltip.add(getOrCreateTagCompound(stack).getString("BARREL"));
+		tooltip.add(getOrCreateTagCompound(stack).getString("MECHANISM"));
+		tooltip.add(getOrCreateTagCompound(stack).getString("CHAMBER"));
+		tooltip.add(getOrCreateTagCompound(stack).getString("STOCK"));
 		if(getOrCreateTagCompound(stack).getBoolean("isLoaded")) {
 			tooltip.add("Loaded");
 		}
@@ -56,7 +61,11 @@ public class ItemGun extends ItemBase {
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
 		ItemStack stack = playerIn.getHeldItem(handIn);
-		if(getOrCreateTagCompound(stack).getBoolean("isLoaded")) {
+
+		if(ActionType.AUTO
+				.equals(((IMechanism) GunPartRegistry.getPart(getOrCreateTagCompound(stack).getString("MECHANISM")))
+						.getActionType())
+				|| getOrCreateTagCompound(stack).getBoolean("isLoaded")) {
 			playerIn.setActiveHand(handIn);
 		}
 		else {
@@ -72,16 +81,14 @@ public class ItemGun extends ItemBase {
 
 	@Override
 	public void onUsingTick(ItemStack stack, EntityLivingBase entityLiving, int count) {
-		// if(entityLiving instanceof EntityPlayer) {
-		// EntityPlayer playerIn = (EntityPlayer) entityLiving;
-		// if(getOrCreateTagCompound(stack).getString("action_type") == "auto") {
-		// EntityBullet bullet = new EntityBullet(playerIn.getEntityWorld(), playerIn);
-		// bullet.shoot(playerIn, playerIn.getPitchYaw().x,
-		// playerIn.getRotationYawHead(), playerIn.getEyeHeight(),
-		// 3f, 0);
-		// playerIn.getEntityWorld().spawnEntity(bullet);
-		// }
-		// }
+		if(((IMechanism) GunPartRegistry.getPart(getOrCreateTagCompound(stack).getString("MECHANISM")))
+				.getActionType() == ActionType.AUTO) {
+			ItemStack ammo = this.findAmmo((EntityPlayer) entityLiving, stack);
+			if(!ammo.isEmpty()) {
+				ammo.shrink(1);
+				shoot(entityLiving.getEntityWorld(), entityLiving);
+			}
+		}
 	}
 
 	@Override
@@ -92,27 +99,27 @@ public class ItemGun extends ItemBase {
 		}
 		if(getOrCreateTagCompound(stack).getBoolean("isLoaded")) {
 			if(entityLiving instanceof EntityPlayer) {
-				EntityPlayer playerIn = (EntityPlayer) entityLiving;
-
-				EntityBullet bullet = new EntityBullet(worldIn, playerIn);
-				bullet.shoot(playerIn, playerIn.getPitchYaw().x, playerIn.getRotationYawHead(), playerIn.getEyeHeight(),
-						3f, 0);
-				worldIn.spawnEntity(bullet);
+				shoot(worldIn, entityLiving);
 				stack.getTagCompound().setBoolean("isLoaded", false);
-				// if(getOrCreateTagCompound(stack).getString("action_type") == "semi") {
-				// ItemStack ammo = this.findAmmo(playerIn, stack);
-				// if(!ammo.isEmpty()) {
-				// ammo.shrink(1);
-				// getOrCreateTagCompound(stack).setBoolean("isLoaded", true);
-				// }
-				// }
+
 			}
 		}
 	}
 
+	private void shoot(World worldIn, EntityLivingBase entityLiving) {
+		EntityPlayer playerIn = (EntityPlayer) entityLiving;
+
+		EntityBullet bullet = new EntityBullet(worldIn, playerIn);
+		bullet.shoot(playerIn, playerIn.getPitchYaw().x, playerIn.getRotationYawHead(), playerIn.getEyeHeight(), 3f, 0);
+		worldIn.spawnEntity(bullet);
+	}
+
 	@Override
 	public int getMaxItemUseDuration(ItemStack stack) {
-		if(getOrCreateTagCompound(stack).getBoolean("isLoaded")) {
+		if(ActionType.AUTO
+				.equals(((IMechanism) GunPartRegistry.getPart(getOrCreateTagCompound(stack).getString("MECHANISM")))
+						.getActionType())
+				|| getOrCreateTagCompound(stack).getBoolean("isLoaded")) {
 			return 72000;
 		}
 		return super.getMaxItemUseDuration(stack);
