2825bb8713c2b732d9df45c7e1236fb617884a92
warlordjones <warlordjones@gmail.com>
2019-02-16 23:47:10 +0000

    Add capacity/fill information to steam tool tooltip

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/armory/items/ItemSteamPickaxe.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/armory/items/ItemSteamPickaxe.java
@@ -10,6 +10,7 @@ import com.teamacronymcoders.base.IModAware;
 import com.teamacronymcoders.base.client.models.IHasModel;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.*;
@@ -22,115 +23,134 @@ import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.fluids.capability.templates.FluidHandlerItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import xyz.brassgoggledcoders.steamagerevolution.modules.armory.ModuleArmory;
 
 public class ItemSteamPickaxe extends ItemPickaxe implements IHasModel, IModAware {
 
-    public static final int steamUsePerBlock = 10;
-    boolean creativeTabSet = false;
-    int capacity;
-    String name;
-    private IBaseMod mod;
+	public static final int steamUsePerBlock = 10;
+	boolean creativeTabSet = false;
+	int capacity;
+	String name;
+	private IBaseMod mod;
 
-    public ItemSteamPickaxe(String name, int capacity) {
-        super(ModuleArmory.STEAM);
-        setTranslationKey(name);
-        this.capacity = capacity;
-        this.name = name;
-    }
+	public ItemSteamPickaxe(String name, int capacity) {
+		super(ModuleArmory.STEAM);
+		setTranslationKey(name);
+		this.capacity = capacity;
+		this.name = name;
+	}
 
-    @Override
-    public List<String> getModelNames(List<String> modelNames) {
-        modelNames.add(name);
-        return modelNames;
-    }
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, ITooltipFlag advanced) {
+		FluidHandlerItemStack internal = (FluidHandlerItemStack) stack
+				.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
+		FluidStack fluid = internal.getFluid();
+		if(fluid == null) {
+			tooltip.add("0mB/" + capacity + "mB");
+		}
+		else {
+			tooltip.add(fluid.getLocalizedName());
+			tooltip.add(fluid.amount + "mB/" + capacity + "mB");
+		}
+	}
 
-    @Override
-    public ICapabilityProvider initCapabilities(ItemStack stack, NBTTagCompound nbt) {
-        return new FluidHandlerItemStack(stack, capacity) {
-            @Override
-            public boolean canFillFluidType(FluidStack fluid) {
-                return FluidRegistry.getFluidName(fluid).equals("steam");
-            }
-        };
-    }
+	@Override
+	public List<String> getModelNames(List<String> modelNames) {
+		modelNames.add(name);
+		return modelNames;
+	}
 
-    @Override
-    public float getDestroySpeed(ItemStack stack, IBlockState state) {
-        FluidHandlerItemStack internal = (FluidHandlerItemStack) stack
-                .getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
-        if (internal.getFluid() != null && internal.getFluid().amount >= steamUsePerBlock) {
-            return super.getDestroySpeed(stack, state);
-        } else {
-            return 0.0F;
-        }
-    }
+	@Override
+	public ICapabilityProvider initCapabilities(ItemStack stack, NBTTagCompound nbt) {
+		return new FluidHandlerItemStack(stack, capacity) {
+			@Override
+			public boolean canFillFluidType(FluidStack fluid) {
+				return FluidRegistry.getFluidName(fluid).equals("steam");
+			}
+		};
+	}
 
-    @Override
-    public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos,
-                                    EntityLivingBase entityLiving) {
-        FluidHandlerItemStack internal = (FluidHandlerItemStack) stack
-                .getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
-        if (internal.getFluid() != null && internal.getFluid().amount >= steamUsePerBlock) {
-            internal.drain(steamUsePerBlock, true);
-            return true;
-        } else {
-            return false;
-        }
-    }
+	@Override
+	public float getDestroySpeed(ItemStack stack, IBlockState state) {
+		FluidHandlerItemStack internal = (FluidHandlerItemStack) stack
+				.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
+		if(internal.getFluid() != null && internal.getFluid().amount >= steamUsePerBlock) {
+			return super.getDestroySpeed(stack, state);
+		}
+		else {
+			return 0.0F;
+		}
+	}
 
-    @Override
-    public double getDurabilityForDisplay(ItemStack stack) {
-        FluidHandlerItemStack internal = (FluidHandlerItemStack) stack
-                .getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
-        return 1.0D - ((double) internal.getFluid().amount / capacity);
+	@Override
+	public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos,
+			EntityLivingBase entityLiving) {
+		FluidHandlerItemStack internal = (FluidHandlerItemStack) stack
+				.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
+		if(internal.getFluid() != null && internal.getFluid().amount >= steamUsePerBlock) {
+			internal.drain(steamUsePerBlock, true);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 
-    }
+	@Override
+	public double getDurabilityForDisplay(ItemStack stack) {
+		FluidHandlerItemStack internal = (FluidHandlerItemStack) stack
+				.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
+		return 1.0D - ((double) internal.getFluid().amount / capacity);
 
-    @Override
-    public boolean showDurabilityBar(ItemStack stack) {
-        FluidHandlerItemStack internal = (FluidHandlerItemStack) stack
-                .getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
-        return internal.getFluid() != null;
-    }
+	}
 
-    @Override
-    @ParametersAreNonnullByDefault
-    public void getSubItems(@Nullable CreativeTabs tab, NonNullList<ItemStack> subItems) {
-        if (tab != null && tab == getCreativeTab() || tab == CreativeTabs.SEARCH) {
-            subItems.addAll(getAllSubItems(Lists.newArrayList()));
-        }
-    }
+	@Override
+	public boolean showDurabilityBar(ItemStack stack) {
+		FluidHandlerItemStack internal = (FluidHandlerItemStack) stack
+				.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
+		return internal.getFluid() != null;
+	}
 
-    @Override
-    public List<ItemStack> getAllSubItems(List<ItemStack> itemStacks) {
-        itemStacks.add(new ItemStack(this, 1));
-        return itemStacks;
-    }
+	@Override
+	@ParametersAreNonnullByDefault
+	public void getSubItems(@Nullable CreativeTabs tab, NonNullList<ItemStack> subItems) {
+		if(tab != null && tab == getCreativeTab() || tab == CreativeTabs.SEARCH) {
+			subItems.addAll(getAllSubItems(Lists.newArrayList()));
+		}
+	}
 
-    @Override
-    @Nonnull
-    public Item setCreativeTab(@Nonnull CreativeTabs tab) {
-        if (!creativeTabSet) {
-            super.setCreativeTab(tab);
-            creativeTabSet = true;
-        }
-        return this;
-    }
+	@Override
+	public List<ItemStack> getAllSubItems(List<ItemStack> itemStacks) {
+		itemStacks.add(new ItemStack(this, 1));
+		return itemStacks;
+	}
 
-    @Override
-    public IBaseMod getMod() {
-        return mod;
-    }
+	@Override
+	@Nonnull
+	public Item setCreativeTab(@Nonnull CreativeTabs tab) {
+		if(!creativeTabSet) {
+			super.setCreativeTab(tab);
+			creativeTabSet = true;
+		}
+		return this;
+	}
 
-    @Override
-    public void setMod(IBaseMod mod) {
-        this.mod = mod;
-    }
+	@Override
+	public IBaseMod getMod() {
+		return mod;
+	}
 
-    @Override
-    public Item getItem() {
-        return this;
-    }
+	@Override
+	public void setMod(IBaseMod mod) {
+		this.mod = mod;
+	}
+
+	@Override
+	public Item getItem() {
+		return this;
+	}
 
 }
