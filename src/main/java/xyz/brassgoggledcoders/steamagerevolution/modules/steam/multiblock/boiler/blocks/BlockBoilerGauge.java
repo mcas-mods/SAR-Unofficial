c6cf0bdd72fba3c48d77c2730bcc6a20df261dd2
warlordjones <warlordjones@gmail.com>
2019-02-17 12:29:28 +0000

    Testing

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/steam/multiblock/boiler/blocks/BlockBoilerGauge.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/steam/multiblock/boiler/blocks/BlockBoilerGauge.java
@@ -1,6 +1,8 @@
 package xyz.brassgoggledcoders.steamagerevolution.modules.steam.multiblock.boiler.blocks;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
@@ -15,8 +17,11 @@ import xyz.brassgoggledcoders.steamagerevolution.utils.multiblock.BlockMultibloc
 
 public class BlockBoilerGauge extends BlockMultiblockBase<TileEntityBoilerGauge> {
 
+	public static final PropertyBool MULTIBLOCK_FORMED = PropertyBool.create("multiblock_formed");
+
 	public BlockBoilerGauge(Material material, String name) {
 		super(material, name);
+		this.setDefaultState(this.blockState.getBaseState().withProperty(MULTIBLOCK_FORMED, false));
 	}
 
 	@Override
@@ -49,6 +54,25 @@ public class BlockBoilerGauge extends BlockMultiblockBase<TileEntityBoilerGauge>
 	@SideOnly(Side.CLIENT)
 	public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess worldIn, BlockPos pos, EnumFacing side) {
 		return worldIn.getBlockState(pos.offset(side)).getBlock() == this ? false : true;
+	}
+
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return 0;
+	}
+
+	@Override
+	public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
+		TileEntityBoilerGauge te = this.getTileEntity(worldIn, pos).get();
+		if(te.isConnected()) {
+			return state.withProperty(MULTIBLOCK_FORMED, te.getMultiblockController().isAssembled());
+		}
+		return state;
+	}
+
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, MULTIBLOCK_FORMED);
 	}
 
 }
