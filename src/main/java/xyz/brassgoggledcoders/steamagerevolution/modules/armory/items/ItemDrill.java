cd8922cf6c9abd63afb5a19b04faec37100e83fe
Arona Jones <aj@aronajones.com>
2019-02-18 12:17:09 +0000

    Drills functional

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/armory/items/ItemDrill.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/armory/items/ItemDrill.java
@@ -1,5 +1,6 @@
 package xyz.brassgoggledcoders.steamagerevolution.modules.armory.items;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -14,6 +15,7 @@ import com.teamacronymcoders.base.IModAware;
 import com.teamacronymcoders.base.client.models.IHasModel;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
@@ -26,22 +28,21 @@ import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.oredict.OreDictionary;
 import xyz.brassgoggledcoders.steamagerevolution.utils.TinkersUtils;
 
 public class ItemDrill extends ItemTool implements IHasModel, IModAware  {
-
-	private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(Blocks.DIRT, Blocks.STONE);
 	
 	boolean creativeTabSet = false;
 	String name;
 	private IBaseMod mod;
 	
 	public ItemDrill(String name, ToolMaterial material) {
-		super(material, EFFECTIVE_ON);
+		super(material, new HashSet<>());
 		setTranslationKey(name);
 		this.name = name;
+		this.setHarvestLevel("pickaxe", material.getHarvestLevel());
+		this.setHarvestLevel("shovel", material.getHarvestLevel());
 	}
 	
 	@Override
@@ -82,12 +83,6 @@ public class ItemDrill extends ItemTool implements IHasModel, IModAware  {
 	public Item getItem() {
 		return this;
 	}
-
-	@Override
-    public Set<String> getToolClasses(ItemStack stack)
-    {
-        return Sets.newHashSet("pickaxe", "shovel");
-    }
 	
 	@Override
 	public boolean onBlockStartBreak(ItemStack stack, BlockPos pos, EntityPlayer player)
@@ -100,9 +95,19 @@ public class ItemDrill extends ItemTool implements IHasModel, IModAware  {
 	}
 	
 	@Override
-	public boolean canHarvestBlock(IBlockState blockIn)
+	public boolean canHarvestBlock(IBlockState blockIn, ItemStack stack)
     {
-		return (Items.STONE_PICKAXE.canHarvestBlock(blockIn) || Items.STONE_SHOVEL.canHarvestBlock(blockIn));
+		Block block = blockIn.getBlock();
+		for(int id : OreDictionary.getOreIDs(new ItemStack(block))) {
+			if(OreDictionary.getOreName(id).contains("ore")) {
+				return false;
+			}
+		}
+		if (block == Blocks.OBSIDIAN)
+        {
+            return this.toolMaterial.getHarvestLevel() == 3;
+        }
+		return Items.STONE_SHOVEL.canHarvestBlock(blockIn, stack);
     }
 
 	//Elevate to public
