1a70207911d5b0dd10a6b6ac17bb7c3909937e3a
Arona Jones <aj@aronajones.com>
2019-04-16 23:03:41 +0100

    More drill work

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/mining/drill/ControllerDrill.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/mining/drill/ControllerDrill.java
@@ -1,21 +1,15 @@
 package xyz.brassgoggledcoders.steamagerevolution.modules.mining.drill;
 
-import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 
 import org.apache.commons.lang3.tuple.Pair;
 
-import com.mojang.authlib.GameProfile;
-import com.teamacronymcoders.base.multiblock.validation.IMultiblockValidator;
+import com.google.common.collect.Lists;
 
-import net.minecraft.block.material.Material;
-import net.minecraft.init.Blocks;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldServer;
-import net.minecraftforge.common.util.FakePlayer;
-import net.minecraftforge.common.util.FakePlayerFactory;
 import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fml.common.FMLLog;
 import xyz.brassgoggledcoders.steamagerevolution.SteamAgeRevolution;
 import xyz.brassgoggledcoders.steamagerevolution.utils.fluids.FluidTankSingleSmart;
 import xyz.brassgoggledcoders.steamagerevolution.utils.inventory.InventoryMachine;
@@ -26,31 +20,47 @@ import xyz.brassgoggledcoders.steamagerevolution.utils.multiblock.SARMultiblockI
 
 public class ControllerDrill extends SARMultiblockInventory {
 
-	private static final GameProfile profile = new GameProfile(null, "[" + SteamAgeRevolution.MODNAME + "]");
+	//private static final GameProfile profile = new GameProfile(null, "[" + SteamAgeRevolution.MODNAME + "]");
 	
-	public BlockPos minimumInteriorPos;
-	public BlockPos maximumInteriorPos;
+	ArrayList<BlockPos> positions = Lists.newArrayList();
+	int currentPosition = 0;
 	
 	protected ControllerDrill(World world) {
 		super(world);
-		this.setInventory(new InventoryMachine(new InventoryPieceItem(new ItemStackHandlerSmart(1, this), 0, 0), null, new InventoryPieceItem(new ItemStackHandlerSmart(9, this), new int[] {0}, new int[] {0}), null, new InventoryPieceFluid(new FluidTankSingleSmart(Fluid.BUCKET_VOLUME * 16, "steam", this), 13, 9)));
+		this.setInventory(new InventoryMachine(new InventoryPieceItem(new ItemStackHandlerSmart(1, this), 0, 0), null, new InventoryPieceItem(new ItemStackHandlerSmart(9, this), new int[] {0,0,0,0,0,0,0,0,0}, new int[] {0,0,0,0,0,0,0,0,0}), null, new InventoryPieceFluid(new FluidTankSingleSmart(Fluid.BUCKET_VOLUME * 16, "steam", this), 13, 9)));
 		
 	}
 	
 	@Override
 	protected void onMachineAssembled() {
 		Pair<BlockPos, BlockPos> interiorPositions = com.teamacronymcoders.base.util.PositionUtils
-				.shrinkPositionCubeBy(getMinimumCoord(), getMaximumCoord(), 1);
-		minimumInteriorPos = interiorPositions.getLeft();
-		maximumInteriorPos = interiorPositions.getRight();
+				.shrinkPositionCubeBy(getMinimumCoord(), new BlockPos(getMaximumCoord().getX(), getMinimumCoord().getY(), getMaximumCoord().getZ()), 1);
+		BlockPos minimumInteriorPosXZ = interiorPositions.getLeft();
+		BlockPos maximumInteriorPosXZ = interiorPositions.getRight();
+		BlockPos.getAllInBox(minimumInteriorPosXZ.down(2), maximumInteriorPosXZ.down(8)).forEach(pos -> positions.add(pos));
 		super.onMachineAssembled();
 	}
 	
 	@Override
 	protected boolean updateServer() {
-		WeakReference<FakePlayer> fakePlayer = new WeakReference<FakePlayer>(FakePlayerFactory.get((WorldServer) WORLD, profile));
-		for(BlockPos pos : BlockPos.getAllInBoxMutable(minimumInteriorPos, maximumInteriorPos)) {
-			WORLD.getBlockState(pos).getBlock().removedByPlayer(WORLD.getBlockState(pos), WORLD, pos, fakePlayer.get(), fakePlayer.get().canHarvestBlock(WORLD.getBlockState(pos)));
+		//WeakReference<FakePlayer> fakePlayer = new WeakReference<FakePlayer>(FakePlayerFactory.get((WorldServer) WORLD, profile));
+		if(this.getCurrentProgress() >= 20) {
+			if(currentPosition < positions.size()) {
+				BlockPos position = positions.get(currentPosition);
+				//FMLLog.warning(WORLD.getBlockState(position).getBlock().getLocalizedName());
+				if(!WORLD.isAirBlock(position)) {
+					WORLD.destroyBlock(position, true);
+					SteamAgeRevolution.instance.getLogger().devInfo("Mining " + position.toString());
+				}
+				currentPosition++;
+			}
+			else {
+				currentPosition = 0;
+			}
+			this.currentTicks = 0;
+		} 
+		else {
+			this.currentTicks++;
 		}
 		return false;
 	}
@@ -78,12 +88,12 @@ public class ControllerDrill extends SARMultiblockInventory {
 
 	@Override
 	public int getMaximumYSize() {
-		return 16;
+		return 1;
 	}
 	
 	@Override
 	public int getMinimumYSize() {
-		return 4;
+		return 1;
 	}
 	
 	@Override
@@ -94,31 +104,6 @@ public class ControllerDrill extends SARMultiblockInventory {
 	@Override
 	public int getMinimumZSize() {
 		return 4;
-	}
-	
-	@Override
-	protected boolean isBlockGoodForSides(World world, int x, int y, int z,
-			IMultiblockValidator validatorCallback) {
-		Material mat = world.getBlockState(new BlockPos(x,y,z)).getMaterial();
-		return mat == Material.ROCK || mat == Material.GROUND || mat == Material.GRASS || mat == Material.SAND || world.isAirBlock(new BlockPos(x,y,z));
-	}
-	
-	@Override
-	protected boolean isBlockGoodForInterior(World world, int x, int y, int z,
-			IMultiblockValidator validatorCallback) {
-		return this.isBlockGoodForSides(world, x, y, z, validatorCallback);
-	}
-	
-	@Override
-	protected boolean isBlockGoodForTop(World world, int x, int y, int z,
-			IMultiblockValidator validatorCallback) {
-		return this.isBlockGoodForSides(world, x, y, z, validatorCallback);
-	}
-	
-	@Override
-	protected boolean isBlockGoodForBottom(World world, int x, int y, int z,
-			IMultiblockValidator validatorCallback) {
-		return this.isBlockGoodForSides(world, x, y, z, validatorCallback);
 	}
 
 }
