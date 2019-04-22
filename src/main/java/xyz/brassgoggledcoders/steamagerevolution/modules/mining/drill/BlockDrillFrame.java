a594685f9a4b3c9188f03252f876755592dc1e6f
Arona Jones <aj@aronajones.com>
2019-04-22 13:22:33 +0100

    Drill textures

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/mining/drill/BlockDrillFrame.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/modules/mining/drill/BlockDrillFrame.java
@@ -3,7 +3,14 @@ package xyz.brassgoggledcoders.steamagerevolution.modules.mining.drill;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import xyz.brassgoggledcoders.steamagerevolution.utils.multiblock.BlockMultiblockBase;
 
 public class BlockDrillFrame extends BlockMultiblockBase<TileEntityDrillFrame> {
@@ -21,5 +28,31 @@ public class BlockDrillFrame extends BlockMultiblockBase<TileEntityDrillFrame> {
 	public TileEntity createTileEntity(World world, IBlockState blockState) {
 		return new TileEntityDrillFrame();
 	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public BlockRenderLayer getRenderLayer() {
+		return BlockRenderLayer.CUTOUT;
+	}
 
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
+	//@Override
+	//@SideOnly(Side.CLIENT)
+	//public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess worldIn, BlockPos pos, EnumFacing side) {
+	//	return worldIn.getBlockState(pos.offset(side)).getBlock() == this ? false : true;
+	//}
+
+	@Override
+	public EnumBlockRenderType getRenderType(IBlockState state) {
+		return EnumBlockRenderType.MODEL;
+	}
 }
