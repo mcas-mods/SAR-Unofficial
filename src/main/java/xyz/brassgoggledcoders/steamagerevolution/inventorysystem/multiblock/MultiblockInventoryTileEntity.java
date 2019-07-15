bba1570078a2efcb1ebc0938cce54a32a3c667bc
warlordjones <warlordjones@gmail.com>
2019-07-15 21:19:40 +0100

    Multiblock TEs implement IHasInventory
    Passing through to Controller instance

--- src/main/java/xyz/brassgoggledcoders/steamagerevolution/inventorysystem/multiblock/MultiblockInventoryTileEntity.java
+++ src/main/java/xyz/brassgoggledcoders/steamagerevolution/inventorysystem/multiblock/MultiblockInventoryTileEntity.java
@@ -3,14 +3,17 @@ package xyz.brassgoggledcoders.steamagerevolution.inventorysystem.multiblock;
 import javax.annotation.Nonnull;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import xyz.brassgoggledcoders.steamagerevolution.inventorysystem.IHasInventory;
 import xyz.brassgoggledcoders.steamagerevolution.inventorysystem.recipe.InventoryRecipe;
 import xyz.brassgoggledcoders.steamagerevolution.inventorysystem.recipe.MultiblockRecipe;
 import xyz.brassgoggledcoders.steamagerevolution.utils.multiblock.SARMultiblockTileBase;
 
 public abstract class MultiblockInventoryTileEntity<T extends MultiblockRecipe<? extends InventoryRecipe>>
-		extends SARMultiblockTileBase<T> {
+		extends SARMultiblockTileBase<T> implements IHasInventory<InventoryRecipe> {
 	// Handles sync on world load
 	@Nonnull
 	@Override
@@ -29,5 +32,36 @@ public abstract class MultiblockInventoryTileEntity<T extends MultiblockRecipe<?
 			this.getMultiblockController().onAttachedPartWithMultiblockData(this, tag);
 		}
 		this.readFromNBT(tag);
+	}
+
+	@Override
+	public String getName() {
+		return this.getMultiblockController().getName();
+	}
+
+	@Override
+	public InventoryRecipe getInventory() {
+		return this.getMultiblockController().getInventory();
+	}
+
+	@Override
+	public void setInventory(InventoryRecipe inventory) {
+		// TODO
+		// return this.getMultiblockController().setInventory(inventory);
+	}
+
+	@Override
+	public void markMachineDirty() {
+		this.getMultiblockController().markMachineDirty();
+	}
+
+	@Override
+	public World getMachineWorld() {
+		return super.getWorld();
+	}
+
+	@Override
+	public BlockPos getMachinePos() {
+		return super.getPos();
 	}
 }
